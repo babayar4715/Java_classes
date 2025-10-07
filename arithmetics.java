2dd6d8fc9affe62f85711f0b0c00f8fb24f10d5b
Babaiar <119310971+babayar4715@users.noreply.github.com>
2025-10-06 20:58:38 -0500

    Add files via upload

---
+++ arithmetics.java
@@ -0,0 +1,34 @@
+package main;
+
+public class arithmetics {
+    public static void main(String[] args) {
+        //int x = 10;
+        //int y = 3;
+        //int z;
+        //z = x -y;
+        //z= y*x;
+       // z= x;
+
+        //x+= y;
+        //x *= y;
+
+        // Increment and Decrement operators
+
+       /* int x = 1;
+        //x++;
+        //x += 1;
+        x-=1;
+        x--;*/
+
+        // ORDER OF OPERATORS [ P-E-M-D-A-S]
+
+        double result = 3+4*(7-5)/10.0+6+9-5/4.5*6+5;
+        System.out.println(result);
+
+
+
+
+       // System.out.println(x);
+
+    }
+}
